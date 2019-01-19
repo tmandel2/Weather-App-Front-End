919b20ae748740ce23f77a903fb321ee0d97404c
Tim Mandel <timothy.mandel@gmail.com>
2019-01-19 16:43:34 -0600

    Basic app works

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,7 +1,7 @@
 console.log('hi');
 
 
-weatherApp = (zipCode) => {
+currentWeatherApp = (zipCode) => {
 	console.log('Weather app function');
 	$.ajax({
 		url: `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=e131115e6491a3ae223530b75af706a0`,
@@ -11,19 +11,65 @@ weatherApp = (zipCode) => {
 			let farenheit = (kelvin - 273.15) * 9 / 5 + 32;
 			return Math.round(farenheit);
 		},
+		iconChooser (data) {
+			let id = data.weather[0].id;
+			if (id < 300) {
+				return '11d';
+			} else if (id < 500) {
+				return '09d';
+			} else if (id < 505) {
+				return '10d';
+			} else if (id === 511) {
+				return '13d';
+			} else if (id < 532) {
+				return '09d';
+			} else if (id < 623) {
+				return '13d';
+			} else if (id < 782) {
+				return '50d';
+			} else if (id < 801) {
+				return '01d';
+			} else if (id === 801) {
+				return '02d';
+			} else if (id === 802) {
+				return '03d';
+			} else if (id === 803 || id === 804) {
+				return '04d';
+			}
+		},
 		success (weatherData) {
-			// let farenheit = (weatherData.main.temp - 273.15) * 9 / 5 + 32
 			$('#city-name').text(weatherData.name);
 			$('#temp').text(`Currently: ${this.makeFarenheit(weatherData.main.temp)}F`);
-			$('#descriptor').text(weatherData.weather[0].description);
+			$('#descriptor').text(weatherData.weather[0].description.toUpperCase());
 			$('#high-temp').text(`Today's high: ${this.makeFarenheit(weatherData.main.temp_max)}F`);
 			$('#low-temp').text(`Today's low: ${this.makeFarenheit(weatherData.main.temp_min)}F`);
+			$('#current-weather-photo').attr('src', `http://openweathermap.org/img/w/${this.iconChooser(weatherData)}.png`);
 		},
 		fail (err) {
 			console.error(err);
 		}
 	})
 }
+
+// forecastWeatherApp = (zipCode) => {
+// 	console.log('Forecast');
+// 	$.ajax({
+// 		url: `api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=e131115e6491a3ae223530b75af706a0`,
+// 		method: 'GET',
+// 		dataType: 'json',
+// 		success (forecastData) {
+// 			$('#city-name').text(weatherData.name);
+// 			$('#temp').text(`Currently: ${this.makeFarenheit(weatherData.main.temp)}F`);
+// 			$('#descriptor').text(weatherData.weather[0].description.toUpperCase());
+// 			$('#high-temp').text(`Today's high: ${this.makeFarenheit(weatherData.main.temp_max)}F`);
+// 			$('#low-temp').text(`Today's low: ${this.makeFarenheit(weatherData.main.temp_min)}F`);
+// 			$('#current-weather-photo').attr('src', `http://openweathermap.org/img/w/${this.iconChooser(weatherData)}.png`);
+// 		},
+// 		fail (err) {
+// 			console.error(err);
+// 		}
+// 	})
+// }
 
 
 
@@ -34,5 +80,6 @@ weatherApp = (zipCode) => {
 
 $('#button').on('click', (e) => {
 	e.preventDefault();
-	weatherApp($('#zip-code').val());
+	currentWeatherApp($('#zip-code').val());
+	// forecastWeatherApp($('#zip-code').val());
 });
